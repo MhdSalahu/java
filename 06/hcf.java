9fa6067b2f6d14bb5fcafb8222bd04bc1a3184bf
Mhd_Salahu <salahu.m4u@gmail.com>
2023-03-01 00:23:01 +0700

    Sixth prgrm modified

--- 06/hcf.java
+++ 06/hcf.java
@@ -1,37 +1,35 @@
 import java.util.Scanner;
 
-class Main{
-    static int hcf(int n1,int n2)
-    {
-        int rem =1;
-        if(n1>n2)
-        {
-            while(rem!=0)
-            {
-                rem=n1 %n2;
-                n1=n2;
-                n2 = rem;
-            }
-            return n1;
-        }
-        else{
-            while(rem!=0)
-            {
-                rem =n2%n1;
-                n2=n1;
-                n1=rem;
-            }
-            return n2;
-        }
-    }
-    public static void main(String args[])
-    {
-        Scanner number = new Scanner(System.in);
-        System.out.println("Enter 2 numbers");
-        int n1 = number.nextInt();
-        int n2 = number.nextInt();
-        System.out.println("HCF :" + hcf(n1,n2));
-        System.out.println("LCM :" + (n1*n2)/hcf(n1,n2));
+class HcfLcm {
+    public static void main(String args[]) {
 
+        // declare variables
+        int temp1, temp2, a, b, temp, hcf, lcm;
+
+        // Scanner class object to read input values
+        Scanner scanner = new Scanner(System.in);
+
+        // read two numbers from user
+        System.out.print("Enter First Number: ");
+        a = scanner.nextInt();
+        System.out.print("Enter Second Number: ");
+        b = scanner.nextInt();
+
+        // find hcf
+        temp1 = a;
+        temp2 = b;
+        while (temp2 != 0) {
+            temp = temp2;
+            temp2 = temp1 % temp2;
+            temp1 = temp;
+        }
+        hcf = temp1;
+
+        // find lcm
+        lcm = (a * b) / hcf;
+
+        // display hcf and lcm
+        System.out.println("HCF of input numbers: " + hcf);
+        System.out.println("LCM of input numbers: " + lcm);
     }
 }
